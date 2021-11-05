ff558d5852a6b02afb9428542eb31e8ca7fac8a7
RomanSharipov <roman.sharipov.1990@gmail.com>
2021-11-05 22:36:19 +0500

    Revert "changed _sensevityX"
    This reverts commit cf252bd7dc64b185d28dce82176b4edbeb6e0053.

--- Assets/Scripts/ArabMouseLook.cs
+++ Assets/Scripts/ArabMouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class ArabMouseLook : MonoBehaviour
 {
-    [SerializeField] private float _sensevityX = 5f;
+    [SerializeField] private float _sensevityX = 3f;
 
     private float _mouseX;
 
--- Assets/Scripts/SwitcherSiren.cs
+++ Assets/Scripts/SwitcherSiren.cs
@@ -42,7 +42,6 @@ public class SwitcherSiren : MonoBehaviour
             _thiefInside = true;
             SwitchOn();
             StartCoroutine(AddVolume());
-
         }
     }
 
